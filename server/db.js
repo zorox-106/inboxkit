1b95e1c68b8bc8dba75e6483debf7006e8206ca2
zorox-106 <trajath545@gmail.com>
2026-07-13 14:27:04 +0530

    chore: make DB_PATH configurable via env var

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 
-const DB_PATH = path.join(__dirname, '..', 'grid.db');
+const DB_PATH = process.env.DB_PATH || path.join(__dirname, '..', 'grid.db');
 const db = new Database(DB_PATH);
 
 // Enable WAL mode for better concurrent read performance
